1fe36a391fba154d343d23f94307d46c798f1834
Shireen Zoaby <shireen.zoaby@gmail.com>
2017-02-19 17:26:47 +0200

    adds a list of books to the books database
    Relates to #2

--- database/db_build.sql
+++ database/db_build.sql
@@ -7,7 +7,14 @@ CREATE TABLE IF NOT EXISTS books (
     book_name    TEXT       NOT NULL,
     year        INTEGER,
     max_reservation_time   INTEGER,
-    library     TEXT           
+    library     TEXT
 );
+
+INSERT INTO books (book_name, year, max_reservation_time, library) VALUES ('Javascript: The Good Parts', 2008, 14, 'The British Library');
+INSERT INTO books (book_name, year, max_reservation_time, library) VALUES ('Linux in a Nutshell', 2009, 7, 'Abu-Salma library');
+INSERT INTO books (book_name, year, max_reservation_time, library) VALUES ('PostgreSQL Server Programming - Second Edition', 2015, 10, 'The British Library');
+INSERT INTO books (book_name, year, max_reservation_time, library) VALUES ('Don''t Make Me Think', 2000, 7, 'Finland national library');
+INSERT INTO books (book_name, year, max_reservation_time, library) VALUES ('Pride and Prejudice', 1813, 21, 'The British Library');
+INSERT INTO books (book_name, year, max_reservation_time, library) VALUES ('Harry Potter and the Philosopher''s Stone', 1998, 14, 'The British Library');
 
 COMMIT;
