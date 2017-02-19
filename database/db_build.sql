408019f4c8466900ff3a2dbb37e2154ef282f0b8
Shireen Zoaby <shireen.zoaby@gmail.com>
2017-02-19 17:52:21 +0200

    adds a mentor's table and posts table
    relates to #3

--- database/db_build.sql
+++ database/db_build.sql
@@ -1,6 +1,6 @@
 BEGIN;
 
-DROP TABLE IF EXISTS books;
+DROP TABLE IF EXISTS books, mentors, posts, likes;
 
 CREATE TABLE IF NOT EXISTS books (
     book_id          SERIAL     PRIMARY KEY,
@@ -16,5 +16,27 @@ INSERT INTO books (book_name, year, max_reservation_time, library) VALUES ('Post
 INSERT INTO books (book_name, year, max_reservation_time, library) VALUES ('Don''t Make Me Think', 2000, 7, 'Finland national library');
 INSERT INTO books (book_name, year, max_reservation_time, library) VALUES ('Pride and Prejudice', 1813, 21, 'The British Library');
 INSERT INTO books (book_name, year, max_reservation_time, library) VALUES ('Harry Potter and the Philosopher''s Stone', 1998, 14, 'The British Library');
+
+CREATE TABLE IF NOT EXISTS mentors (
+    name    TEXT,
+    location    TEXT
+);
+
+INSERT INTO mentors (name, location) VALUES ('Tom', 'London');
+INSERT INTO mentors (name, location) VALUES ('Shireen', 'Nazareth');
+INSERT INTO mentors (name, location) VALUES ('Emily', 'Nazareth');
+INSERT INTO mentors (name, location) VALUES ('Steve', 'London');
+INSERT INTO mentors (name, location) VALUES ('Jack', 'Nazareth');
+
+CREATE TABLE IF NOT EXISTS posts (
+    num    INTEGER,
+    mentor_name    TEXT
+);
+
+INSERT INTO posts (num, mentor_name) VALUES (20, 'Steve');
+INSERT INTO posts (num, mentor_name) VALUES (32, 'Shireen');
+INSERT INTO posts (num, mentor_name) VALUES (44, 'Shireen');
+INSERT INTO posts (num, mentor_name) VALUES (19, 'Tom');
+INSERT INTO posts (num, mentor_name) VALUES (57, 'Shireen');
 
 COMMIT;
