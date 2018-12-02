c736e30d76c2548318edcf1589b8fcb78fc230c4
Matt Davis <matthewpeterdavis@gmail.com>
2018-12-02 12:18:39 +0200

    change num => id and add outputs

--- solution.sql
+++ solution.sql
@@ -3,13 +3,27 @@
   FROM books B
   WHERE max_reservation_time >
   (SELECT AVG(max_reservation_time) FROM books
-  WHERE library = B.library)
+  WHERE library = B.library);
+
+--    book_id |         book_name          | max_reservation_time 
+-- ---------+----------------------------+----------------------
+--        1 | Javascript: The Good Parts |                   21
+--        5 | Pride and Prejudice        |                   21
 
 -- 2.
 -- 2.1.
-  SELECT COUNT(num), posts.mentor_name FROM posts INNER JOIN likes ON post_num=num GROUP BY posts.mentor_name
+  SELECT COUNT(id), posts.mentor_name FROM posts INNER JOIN likes ON post_id=id GROUP BY posts.mentor_name;
+
+
+--  count | mentor_name 
+-- -------+-------------
+--      4 | Steve
+--      4 | Tom
+--      9 | Shireen
+
+
 -- 2.2.
-  SELECT location, post_num FROM mentors INNER JOIN likes ON name=mentor_name
+  SELECT location, post_id FROM mentors INNER JOIN likes ON name=mentor_name;
 
 -- 3. by Emily
 SELECT
@@ -23,11 +37,16 @@ FROM (
 
   FROM mentors auth
   JOIN posts p ON p.mentor_name = auth.name
-  JOIN likes l ON l.post_num = p.num
+  JOIN likes l ON l.post_id = p.id
 
   GROUP BY
-    p.num,
+    p.id,
     auth.location
 ) AS Q1
 
 GROUP BY location;
+
+--  location | avg 
+-- ----------+-----
+--  London   |   4
+--  Nazareth |   3
