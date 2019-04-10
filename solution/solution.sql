4ee86e9c7dd13c60055d5039a10692dc61ab15ae
Lucy E Wright <40594944+lucyewright@users.noreply.github.com>
2019-04-10 11:44:10 +0100

    Update solution.sql
    The solution currently for part 1 is hard coded to just look at the british library (which works for the challenge) but would not work for a more extensive database.
    Credit to Rohan FAC16.

--- solution/solution.sql
+++ solution/solution.sql
@@ -5,6 +5,15 @@
   (SELECT AVG(max_reservation_time) FROM books
   WHERE library = B.library);
 
+-- -- -- -- -- NEW SOLUTION??? -- -- -- -- -- 
+--   SELECT book_id, book_name, max_reservation_time
+--   FROM books B
+--   WHERE max_reservation_time > 
+--   (SELECT averages FROM 
+--    (SELECT AVG(max_reservation_time) 
+--     AS averages, library AS lib from books GROUP BY library) 
+--    AS libraries WHERE library = lib)
+
 -- 2.
 -- 2.1.
   SELECT COUNT(id), posts.mentor_name FROM posts INNER JOIN likes ON post_id=id GROUP BY posts.mentor_name;
